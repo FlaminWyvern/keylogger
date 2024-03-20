2a5c2e06b25a70e51600c498c8d9983b4a5ec190
Josh Philip <jphilip3804@gmail.com>
2024-03-20 13:01:02 -0400

    final changes

--- keylogger.py
+++ keylogger.py
@@ -16,7 +16,9 @@ def keyPressed(key):
                 logKey.seek(logKey.tell()-1, 0)
                 logKey.truncate()
             elif key == keyboard.Key.esc:
-                return False
+                exit(1)
+            elif key == keyboard.Key.shift_l or key == keyboard.Key.shift_r:
+                print("Hold shift to use the secondary keys/uppercase letters")
             else:
                 print("Error getting char")
 
